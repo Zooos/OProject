bf1ddec406a6dee9831dfdcb565e5d4373bf912d
Oleh <kopobai4uk@gmail.com>
2018-12-08 13:38:26 +0200

    admin

--- mysite/myapp/admin.py
+++ mysite/myapp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from .models import Institutions, Event, Comments, MyUser
+
 # Register your models here.
+admin.site.register(Institutions)
+admin.site.register(Event)
+admin.site.register(Comments)
+admin.site.register(MyUser)
